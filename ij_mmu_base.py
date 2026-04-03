49a5d8910a4f6a6b1a370ca620aa730c3f3e0cdf
Namida Verasche <namida.verasche@gmail.com>
2026-04-03 18:01:38 +1300

    Reduced default MMU timeout.

--- ij_mmu_base.py
+++ ij_mmu_base.py
@@ -6,7 +6,7 @@ import time
 class IJM_Base:
     def __init__(self, connection: IJCI_Base):
         self.connection = connection
-        self.command_response_wait_timeout = 1
+        self.command_response_wait_timeout = 0.2
         self.friendly_name = "Base Placeholder"
         self.out_cmd_queue: list[dict[str, str]] = []
 
