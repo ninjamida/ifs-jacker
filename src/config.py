5fd6aea3849571980b98ff715e1c85c7230c3249
Namida Verasche <namida.verasche@gmail.com>
2026-04-08 16:38:05 +1200

    https://github.com/ninjamida/ifs-jacker/issues/1 Unable to live test as I'm mid-print, but fixed in simulation testing. Will merge after live testing.
    As a side effect should also fix https://github.com/ninjamida/ifs-jacker/issues/2

--- src/config.py
+++ src/config.py
@@ -13,6 +13,7 @@ def load_config(core: IJ_Core, comm_mgr: comm.IJ_Comm_Manager):
     try:
         core_data = ini_data.get('core', {})
         core.include_channel_count_in_status = core_data.get('include_channel_count_in_status', 'false') == 'true'
+        core.F13_timeout = int(float(core_data.get('f13_timeout', 0.075)) * 1000)
         force_present_channels = [item.strip() for item in core_data.get('force_present_channels', '').split(',')]
         force_absent_channels = [item.strip() for item in core_data.get('force_absent_channels', '').split(',')]
         if len(force_absent_channels) > 1 or force_absent_channels[0] != '':
--- src/util.py
+++ src/util.py
@@ -2,7 +2,7 @@ import sys, io
 
 SCRIPT_IDENTIFIER = 'IFS Jacker'
 SCRIPT_AUTHOR = 'Namida Verasche (Trumble)'
-SCRIPT_VERSION = '2.1.0'
+SCRIPT_VERSION = '2.1.0-fix-disconnected-ifs-bug'
 
 def get_traceback_string(e: Exception) -> list[str]:
     buf = io.StringIO()
