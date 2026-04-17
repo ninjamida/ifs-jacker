c1a3a6af5b644fa866147c939940934acf9a5943
Namida Verasche <namida.verasche@gmail.com>
2026-04-17 18:25:54 +1200

    0.05sec MMU timeout is too low. While very rare, it does sometimes have timeouts. Increased to 0.06 as default, which seems reliable (but does need longer-term testing before I can say for sure).

--- src/config.py
+++ src/config.py
@@ -16,7 +16,7 @@ def load_config(core: IJ_Core, comm_mgr: comm.IJ_Comm_Manager):
         core.printer_connected_timeout = int(float(core_data.get('timeout', 3)) * 1000)
         core.include_channel_count_in_status = core_data.get('include_channel_count_in_status', 'true') == 'true'
         core.include_peripherals_in_status = core_data.get('include_peripherals_in_status', 'true') == 'true'
-        core.mmu_response_timeout = int(float(core_data.get('mmu_timeout', 0.05)) * 1000)
+        core.mmu_response_timeout = int(float(core_data.get('mmu_timeout', 0.06)) * 1000)
         force_present_channels = [item.strip() for item in core_data.get('force_present_channels', '').split(',')]
         force_absent_channels = [item.strip() for item in core_data.get('force_absent_channels', '').split(',')]
         if len(force_absent_channels) > 1 or force_absent_channels[0] != '':
